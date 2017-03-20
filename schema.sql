50740eb3981e7bb1514ef3d2ab2a89b08279ea3f
ulises-jeremias <ulisescf.24@gmail.com>
2017-03-20 00:04:57 -0300

    Create codDB.sql

---
+++ schema.sql
@@ -0,0 +1,41 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `codDB`
+--
+
+--
+-- Estructura de tabla para la tabla `Users`
+--
+
+CREATE TABLE `Users` (
+  `idUser` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `surname` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(55) NOT NULL,
+  `accounts` varchar(255) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `session` int(11) NOT NULL DEFAULT '0',
+  `registration_date` date NOT NULL,
+  `keyreg` varchar(25) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '2'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indices de la tabla `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`idUser`);
+
+
+ALTER TABLE `Users`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
