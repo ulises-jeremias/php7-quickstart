4eda6d1bae6d045c6a59a77eccaaee6f78ab5b2b
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2017-04-06 14:48:11 -0300

    Update header.php

--- templates/overall/header.php
+++ templates/overall/header.php
@@ -1,6 +1,6 @@
 <head>
-    <title><?php echo APP ?></title>
-    <base href= "<?php URL ?>">
+    <title><?php echo APP; ?></title>
+    <base href= "<?php echo URL; ?>">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
